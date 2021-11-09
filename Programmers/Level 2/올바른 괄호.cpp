52ca170afa7c97f6bf1681dc6c3cf2c9b1f65525
Sun <syoun602@gmail.com>
2021-11-09 20:17:32 +0900

    11/09/2021

---
+++ Programmers/Level 2/올바른 괄호.cpp
@@ -0,0 +1,27 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> st;
+    
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '(')
+            st.push(s[i]);
+        else {
+            if(!st.empty() && st.top() == '(')
+                st.pop();
+            else
+                return false;
+        }
+    }
+    
+    if(!st.empty())
+        answer = false;
+    
+    return answer;
+}
